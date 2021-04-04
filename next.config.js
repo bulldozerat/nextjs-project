3d4049f0ce8e26f2787e4373ce364647d7d00cbb
bulldozerat <bulldozerat@gmail.com>
2021-04-05 01:39:59 +0300

    dev and prd .env logic

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
-module.exports = {
-  env: {
-    testKey: 'test-key',
-  },
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase) => {
+  // npm run dev
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        testKey: 'dev-key',
+      },
+    };
+  }
+
+  return {
+    env: {
+      testKey: 'prd-key',
+    },
+  };
 };
--- pages/staticPaths/[pid].js
+++ pages/staticPaths/[pid].js
@@ -12,33 +12,33 @@ export default function staticPaths({ products }) {
   );
 }
 
-export async function getStaticProps(context) {
-  console.log('context: ', context);
-  const productId = context.params.pid;
-  const product = dummyProducts.find(pr.id === productId);
+// export async function getStaticProps(context) {
+//   console.log('context: ', context);
+//   const productId = context.params.pid;
+//   const product = dummyProducts.find(pr.id === productId);
 
-  return {
-    props: {
-      products,
-    },
-  };
-}
+//   return {
+//     props: {
+//       products,
+//     },
+//   };
+// }
 
 // tells next for which params should pre-render the page
 // TODO research more
-export async function getStaticPaths() {
-  const listWithPregeneratedIds = [
-    {
-      pid: '1',
-      pid: '2',
-      pid: '3',
-    },
-  ];
+// export async function getStaticPaths() {
+//   const listWithPregeneratedIds = [
+//     {
+//       pid: '1',
+//       pid: '2',
+//       pid: '3',
+//     },
+//   ];
 
-  return {
-    paths: listWithPregeneratedIds,
-    fallback: 'blocking',
-    // are there more paths than these paths. If false another id will go to 404 page
-    // if set to blocking it will wait and send the whole html (no loading)
-  };
-}
+//   return {
+//     paths: listWithPregeneratedIds,
+//     fallback: 'blocking',
+//     // are there more paths than these paths. If false another id will go to 404 page
+//     // if set to blocking it will wait and send the whole html (no loading)
+//   };
+// }
