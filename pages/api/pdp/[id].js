7d23aadf5b51a0e73ed58e143e090c878e3bf12d
bulldozerat <bulldozerat@gmail.com>
2021-05-02 13:45:18 +0300

    only GET

--- pages/api/pdp/[id].js
+++ pages/api/pdp/[id].js
@@ -1,7 +1,9 @@
 export default (req, res) => {
+  // By default it works for all (GET, POST, PUT ect)
   if (req.method === 'GET') {
     res.status(200).json({ pdpId: req.query.id });
+  } else {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).json({ message: `Method ${req.method} is not allowed` });
   }
-
-  // res.status(200).json({ name: 'John Doe' });
 };
