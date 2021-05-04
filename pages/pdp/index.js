2691d81b4c60463892ec609faf8a554305a44995
bulldozerat <bulldozerat@gmail.com>
2021-05-04 16:10:23 +0300

    shallow routing

--- pages/pdp/index.js
+++ pages/pdp/index.js
@@ -1,6 +1,20 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 export default function PDP() {
-  return <h1>test</h1>
+  useEffect(() => {
+    // Shallow routing (change URL without running data fetching methods again)
+    router.push('pdp/?counter=10', undefined, { shallow: true });
+  }, []);
+
+  const router = useRouter();
+
+  return (
+    <>
+      <h1>{process.env.NEXT_PUBLIC_FOR_BROWSER_USER}</h1>
+      <Link href='/'>Link</Link>
+      <div onClick={() => router.push('/')}>Router link</div>
+    </>
+  );
 }
-
-
